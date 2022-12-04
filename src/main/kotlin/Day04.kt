aa6dad838915c7e100b9f2474e47d68a18c55a01
Aaron Korver <aaron.korver@gmail.com>
2022-12-04 00:33:32 -0600

    Day 4

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,41 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    solutionFourPartTwo()
+}
+
+fun solutionFourPartOne() {
+    //2-4,6-8
+    val uri = getResourceAsURL("04/input.txt")?.toURI()
+    if (uri != null) {
+        val inputStream = File(uri).inputStream()
+        var count = 0
+        inputStream.bufferedReader().forEachLine {
+            val (a,b)  = it.split(",")
+            .map {
+                it.split("-")
+                    .map(String::toInt)
+                    .let { (a, b) -> (a..b) }
+            }
+            val unionSize = a.union(b).size
+            if(unionSize == a.count() || unionSize == b.count()) count++
+        }
+        print(count)
+
+    }
+}
+
+fun solutionFourPartTwo() {
+    //2-4,6-8
+    val text = getResourceAsText("04/input.txt")
+        val count = text.lines().count {
+            val (a, b) = it.split(",")
+                .map { it.split("-")
+                    .map(String::toInt)
+                    .let { (a,b) -> (a..b) }
+                }
+            a.intersect(b).isNotEmpty()
+        }
+        print(count)
+
+    }
